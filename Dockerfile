531bc4008dadb3f82107c99bbae8c51cc36e7264
Mohammad Jafari <piade@live.com>
2023-06-24 15:12:33 +0330

    move docker code from docker folder yo root

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM python:3.9-slim-buster
 
 RUN apt-get update && apt-get install nginx vim -y --no-install-recommends
-COPY docker/nginx.default /etc/nginx/sites-available/default
+COPY nginx.default /etc/nginx/sites-available/default
 RUN ln -sf /dev/stdout /var/log/nginx/access.log \
     && ln -sf /dev/stderr /var/log/nginx/error.log
 
 RUN mkdir -p /opt/app
-COPY docker/entrypoint.sh /opt/app/
+COPY entrypoint.sh /opt/app/
 COPY ./requirements.txt /opt/app/
 COPY . /opt/app/
 WORKDIR /opt/app
--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,8 @@ python manage.py migrate
 #gunicorn main.wsgi:application --user www-data --bind 0.0.0.0:8010 --workers 3
 #nginx -g "daemon off;"
 
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (python manage.py createsuperuser --no-input)
-fi
+# if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+#     (python manage.py createsuperuser --no-input)
+# fi
 (gunicorn main.wsgi:application --user www-data --bind 0.0.0.0:8000 --workers 3) &
 nginx -g "daemon off;"
