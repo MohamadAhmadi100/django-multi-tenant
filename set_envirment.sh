16d27f48002016c77722abc2ff0b0ad1383bb645
Mohammad Jafari <piade@live.com>
2023-06-24 20:29:12 +0330

    add variable in gitlab ci

--- set_envirment.sh
+++ set_envirment.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
-
-rm -f .env
-touch .env
-
-for i in $(printenv); do
-value=$(awk -FCI_ENV_ '{print $2}' <<< "$i")
-if [ -n "$value" ]; then
-    echo "$value" >> .env
-fi
-done
+##!/bin/sh
+#
+#rm -f .env
+#touch .env
+#
+#for i in $(printenv); do
+#value=$(awk -FCI_ENV_ '{print $2}' <<< "$i")
+#if [ -n "$value" ]; then
+#    echo "$value" >> .env
+#fi
+#done
