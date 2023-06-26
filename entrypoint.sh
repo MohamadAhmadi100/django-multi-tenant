aeda03d3f5d6724caa1fe9ca8058f2c36a689e3f
Mohammad Jafari <piade@live.com>
2023-06-26 11:31:38 +0330

    change some code for test deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3
-nginx -g "daemon off;"
+#gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3
+#nginx -g "daemon off;"
+
+#MJ for test
+exec "$@"
