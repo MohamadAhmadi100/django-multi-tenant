2ed5937914ba9b928c05b066e9d8e5650bc63ddf
Mohammad Jafari <piade@live.com>
2023-06-26 17:57:30 +0330

    disable migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 python manage.py collectstatic --noinput
-python manage.py migrate
+#ask what to do
+#python manage.py migrate
 
 gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3
 nginx -g "daemon off;"
