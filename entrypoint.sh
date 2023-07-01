196417cc44e930d117c98a4129b5da5bf521f694
Mohammad Jafari <piade@live.com>
2023-07-01 19:07:53 +0330

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py collectstatic --noinput
 #ask what to do
 #python manage.py migrate
 
-#gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 300 --log-level debug
+gunicorn main.wsgi:application --bind 0.0.0.0:8001 --workers 3 --timeout 300 --log-level debug
 #python manage.py runserver 0.0.0.0:8000
 
 nginx -g "daemon off;"
