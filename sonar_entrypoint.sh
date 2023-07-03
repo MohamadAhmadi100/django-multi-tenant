e652e8f49b962226ffa0d913f68a9e76957cd9de
Mohammad Jafari <piade@live.com>
2023-07-03 19:44:59 +0330

    change entiry point for sonar

--- sonar_entrypoint.sh
+++ sonar_entrypoint.sh
@@ -5,7 +5,6 @@ python manage.py collectstatic --noinput
 # gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 300 --log-level debug
 
 gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3 --daemon
-nginx -g "daemon off;"
 
 python manage.py makemigrations 
 python manage.py migrate
