3ab0737372a48b09db2265bb1a0b7230b7af0d8e
Mohammad Jafari <piade@live.com>
2023-07-03 17:22:55 +0330

    add connect to database in sonar

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 python manage.py collectstatic --noinput
 #ask what to do
-#python manage.py migrate
+python manage.py makemigrations 
+python manage.py migrate
 
 gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 300 --log-level debug
 nginx -g "daemon off;"
